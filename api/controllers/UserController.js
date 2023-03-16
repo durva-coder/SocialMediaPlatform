def88261b52b905a1ef3b4232d4966d017c07be5
= <=>
2023-03-16 17:34:48 +0530

    sixth commit

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -162,10 +162,6 @@ module.exports = {
          return res.status(200).json({
             status: 200,
             message: 'User logout successfully'
-        }).catch((err)=>{
-            return res.status(400).json({
-                err:err
-            })
         })
         
     },
